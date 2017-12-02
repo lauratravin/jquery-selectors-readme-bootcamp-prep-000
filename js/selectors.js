55fa18458a32b8c189cb6cd4b68f9a4c25201244
Laura M. Travin <laura.travin@gmail.com>
2017-12-02 05:07:28 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -4,3 +4,13 @@ function paragraphSelector(){
   return $('p');
 
 }
+
+function lastImageSelector(){
+  return $('img');
+}
+
+function ninjaBabySelector(){}
+
+function divSelector(){}
+
+function firstListItem(){}
